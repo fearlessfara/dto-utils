e1ee4b95bfcf093ca073176225528c9171da4844
cfaraone <cfaraone@soldo.com>
2022-01-23 16:19:23 +0100

    added missing copyrights (c) messages on classes headers

--- src/test/java/com/fearlessfara/dto/utils/TestClass.java
+++ src/test/java/com/fearlessfara/dto/utils/TestClass.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 fearlessfara (Christian Gennaro Faraone)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fearlessfara.dto.utils;
 
 
@@ -11,20 +27,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestClass {
 
-    static class TestObject {
-        public String field1;
-        Object field2;
-        protected Long field3;
-        private Integer field4;
-        Map<String, Object> map;
-    }
-
     @Test
     public void workingTest() {
         TestObject testObject = new TestObject();
         assertThrows(IllegalArgumentException.class, () -> DTOUtils.notNull(testObject, testObject.field1));
     }
-
 
     @RepeatedTest(10)
     public void testPerformance() {
@@ -42,5 +49,13 @@ public class TestClass {
 
         duration = (end - start);
         System.out.println("time elapsed for reflection: " + duration + " ms");
+    }
+
+    static class TestObject {
+        public String field1;
+        protected Long field3;
+        Object field2;
+        Map<String, Object> map;
+        private Integer field4;
     }
 }
